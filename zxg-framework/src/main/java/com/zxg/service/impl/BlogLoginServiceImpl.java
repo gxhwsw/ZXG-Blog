85b14a12c04ec32bb255c5da0beeadea37d319ff
xvcheng <8483485+xvcheng@user.noreply.gitee.com>
2023-09-19 21:10:12 +0800

    登陆相关

--- zxg-framework/src/main/java/com/zxg/service/impl/BlogLoginServiceImpl.java
+++ zxg-framework/src/main/java/com/zxg/service/impl/BlogLoginServiceImpl.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -59,6 +60,13 @@ public class BlogLoginServiceImpl implements BlogLoginService {
 
     @Override
     public ResponseResult logout() {
-        return null;
+        //获取token 解析获取userid
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        LoginUser loginUser = (LoginUser) authentication.getPrincipal();
+        //获取userid
+        Long userId = loginUser.getUser().getId();
+        //删除redis中的用户信息
+        redisCache.deleteObject("bloglogin:"+userId);
+        return ResponseResult.okResult();
     }
 }
